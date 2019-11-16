0f4e3015fb03b5e9024f3c5538633ed42d9fecb2
Airwaves <prairie5270@gmail.com>
2019-11-16 22:51:12 +0800

    Updates

---
+++ precache-manifest.b0d1f10787fdfae327653ccf8fe5670f.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f8ca0844a7bba4985384fd8c49b87d",
+    "url": "/the-f2e-2nd-mp3-player/index.html"
+  },
+  {
+    "revision": "b5744076cbde38a8b011",
+    "url": "/the-f2e-2nd-mp3-player/static/css/main.562da300.chunk.css"
+  },
+  {
+    "revision": "320a6889556fbbf3d2b7",
+    "url": "/the-f2e-2nd-mp3-player/static/js/2.9215d687.chunk.js"
+  },
+  {
+    "revision": "b5744076cbde38a8b011",
+    "url": "/the-f2e-2nd-mp3-player/static/js/main.801afaf5.chunk.js"
+  },
+  {
+    "revision": "ab4cbd7b28aa26b2e9b5",
+    "url": "/the-f2e-2nd-mp3-player/static/js/runtime-main.758206af.js"
+  },
+  {
+    "revision": "28518cc7046299017b8e256540f71207",
+    "url": "/the-f2e-2nd-mp3-player/static/media/Group 20.28518cc7.svg"
+  },
+  {
+    "revision": "e33d2954d2c37649ddd25bca77c294c6",
+    "url": "/the-f2e-2nd-mp3-player/static/media/Group_5.e33d2954.svg"
+  },
+  {
+    "revision": "87e6bd67815defec3f4c135db8ed1eab",
+    "url": "/the-f2e-2nd-mp3-player/static/media/acoustic-acousticbreeze.87e6bd67.mp3"
+  },
+  {
+    "revision": "06d33c2dc099cb36fe3e5e16a7c84ff9",
+    "url": "/the-f2e-2nd-mp3-player/static/media/acoustic-buddy.06d33c2d.jpg"
+  },
+  {
+    "revision": "cd95474da687fee5d24db6729707f843",
+    "url": "/the-f2e-2nd-mp3-player/static/media/acoustic-buddy.cd95474d.mp3"
+  },
+  {
+    "revision": "0466433ce33c4df06743075bef864477",
+    "url": "/the-f2e-2nd-mp3-player/static/media/acoustic-cute.0466433c.mp3"
+  },
+  {
+    "revision": "9569869286a4e88efa8724d24f50bf47",
+    "url": "/the-f2e-2nd-mp3-player/static/media/acoustic-ukulele.95698692.mp3"
+  },
+  {
+    "revision": "e44d740898376773e55fcffd3cae0432",
+    "url": "/the-f2e-2nd-mp3-player/static/media/acoustic-ukulele.e44d7408.jpg"
+  },
+  {
+    "revision": "277ade9ecf6fd5f1304f2b2489135d71",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-betterdays.277ade9e.mp3"
+  },
+  {
+    "revision": "6f56e4e308a3bb900d5443db67d85f42",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-dubstep.6f56e4e3.mp3"
+  },
+  {
+    "revision": "78d8e9ad461f0f452cfd9104624cd2d2",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-energy.78d8e9ad.mp3"
+  },
+  {
+    "revision": "780462c2501503afc5cadc18e61f00db",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-epic.780462c2.mp3"
+  },
+  {
+    "revision": "004ac20a9fb85e7728778fac0e5a5458",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-funnysong.004ac20a.mp3"
+  },
+  {
+    "revision": "b7303a025fee4c52c32f735833b2bbfa",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-onceagain.b7303a02.mp3"
+  },
+  {
+    "revision": "e0e75e5b7f2eaa57ab39b3a6e5a40f0b",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-slowmotion.e0e75e5b.mp3"
+  },
+  {
+    "revision": "15e0ab24d7ba0cf97151de005bc5225b",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-sunny.15e0ab24.mp3"
+  },
+  {
+    "revision": "aa5b17db6dcb9eb68d06220ada75aa6c",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-tenderness.aa5b17db.mp3"
+  },
+  {
+    "revision": "63cc0a849da3671271a9ccfb7c335d54",
+    "url": "/the-f2e-2nd-mp3-player/static/media/all-tomorrow.63cc0a84.mp3"
+  },
+  {
+    "revision": "5bead52f6165c710428319628907982b",
+    "url": "/the-f2e-2nd-mp3-player/static/media/betterdays.5bead52f.jpg"
+  },
+  {
+    "revision": "d36eb857125fc5647afe099e305bb0cb",
+    "url": "/the-f2e-2nd-mp3-player/static/media/cover.d36eb857.jpg"
+  },
+  {
+    "revision": "661e2d19f45e48483fa1b13c6da2708b",
+    "url": "/the-f2e-2nd-mp3-player/static/media/dubstep.661e2d19.jpg"
+  },
+  {
+    "revision": "d7ca22dfe272a3288235c97126e3aae1",
+    "url": "/the-f2e-2nd-mp3-player/static/media/energy.d7ca22df.jpg"
+  },
+  {
+    "revision": "b93ce3284a8bd65020ef58618c29c0e1",
+    "url": "/the-f2e-2nd-mp3-player/static/media/epic.b93ce328.jpg"
+  },
+  {
+    "revision": "3d40c95a4d45e5a11162481f48fef3e7",
+    "url": "/the-f2e-2nd-mp3-player/static/media/funnysong.3d40c95a.jpg"
+  },
+  {
+    "revision": "037be59f9010ab94d1595a197411abbd",
+    "url": "/the-f2e-2nd-mp3-player/static/media/icon-02-1.037be59f.svg"
+  },
+  {
+    "revision": "f7764fb69266ea12c5aef6264799c7d6",
+    "url": "/the-f2e-2nd-mp3-player/static/media/icon-02.f7764fb6.svg"
+  },
+  {
+    "revision": "9f87575c8f033920ef1af75773a60c18",
+    "url": "/the-f2e-2nd-mp3-player/static/media/onceagain.9f87575c.jpg"
+  },
+  {
+    "revision": "a9542bfca6aa7b61b305921125898563",
+    "url": "/the-f2e-2nd-mp3-player/static/media/play.a9542bfc.svg"
+  },
+  {
+    "revision": "4345a64c02325974ab03d22015eff868",
+    "url": "/the-f2e-2nd-mp3-player/static/media/player.4345a64c.svg"
+  },
+  {
+    "revision": "fbafc176c24e8bd237bf667b9d7b4d85",
+    "url": "/the-f2e-2nd-mp3-player/static/media/search.fbafc176.svg"
+  },
+  {
+    "revision": "22bbae4b608c233a1fe4c2889d0c6f7e",
+    "url": "/the-f2e-2nd-mp3-player/static/media/slowmotion.22bbae4b.jpg"
+  },
+  {
+    "revision": "ce90b1d2917be39345aa682ced6f9e8e",
+    "url": "/the-f2e-2nd-mp3-player/static/media/stop.ce90b1d2.svg"
+  },
+  {
+    "revision": "b54a71c5a6cf6770198f4e89c7e708ef",
+    "url": "/the-f2e-2nd-mp3-player/static/media/sunny.b54a71c5.jpg"
+  },
+  {
+    "revision": "c6890f0a608cca4d833b18cf9d6556cf",
+    "url": "/the-f2e-2nd-mp3-player/static/media/tomorrow.c6890f0a.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/the-f2e-2nd-mp3-player/precache-manifest.b0d1f10787fdfae327653ccf8fe5670f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/the-f2e-2nd-mp3-player/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
